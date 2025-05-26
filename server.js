a4b7a7814aa07fc7b580ea095a3db001ec1a133e
xbze3 <ezranminty@gmail.com>
2025-05-25 21:17:21 -0400

    Update CORS origin

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "*",
+        origin: "https://mintflow.onrender.com/",
         allowedHeaders: ["Authorization", "Content-Type"],
     })
 );
